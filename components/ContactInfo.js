d290f5806b0f15897ce0a03a87b3f66d056d8f31
Ashfak Niloy <ashfakniloy56@gmail.com>
2022-04-23 15:02:41 +0600

    added headers to the post request

--- components/ContactInfo.js
+++ components/ContactInfo.js
@@ -27,6 +27,9 @@ function ContactInfo() {
 
     const res = await fetch(`${API_URL}`, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ name, email, number, message }),
     });
 
