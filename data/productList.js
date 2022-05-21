8ae33a0188337f7b79185e909770b8c45796eb31
Ashfak Niloy <ashfakniloy56@gmail.com>
2022-05-21 12:43:03 +0600

    added services images

--- data/productList.js
+++ data/productList.js
@@ -1,14 +1,54 @@
 export const productList = [
-  { id: 1, name: "product1", img: "/images/products/product1.jpg" },
-  { id: 2, name: "product2", img: "/images/products/product2.jpg" },
-  { id: 3, name: "product3", img: "/images/products/product3.jpg" },
-  { id: 4, name: "product4", img: "/images/products/product4.jpg" },
-  { id: 5, name: "product5", img: "/images/products/product5.jpg" },
-  { id: 6, name: "product6", img: "/images/products/product6.jpg" },
-  { id: 7, name: "product7", img: "/images/products/product7.jpg" },
-  { id: 8, name: "product8", img: "/images/products/product8.jpg" },
-  { id: 9, name: "product9", img: "/images/products/product9.jpg" },
-  { id: 10, name: "product10", img: "/images/products/product10.jpg" },
-  { id: 11, name: "product11", img: "/images/products/product11.jpg" },
-  { id: 12, name: "product12", img: "/images/products/product12.jpg" },
+  { id: 1, name: "cloud computing", img: "/images/products/cloud comp1.png" },
+
+  {
+    id: 2,
+    name: "desktop development",
+    img: "/images/products/desktop dev.png",
+  },
+  {
+    id: 3,
+    name: "web development",
+    img: "/images/products/web development.jpg",
+  },
+  {
+    id: 4,
+    name: "mobile development",
+    img: "/images/products/mobile development.jpg",
+  },
+  {
+    id: 5,
+    name: "digital marketing",
+    img: "/images/products/digital marketing 1.jpg",
+  },
+
+  {
+    id: 6,
+    name: "web development 2",
+    img: "/images/products/web development two.jpg",
+  },
+  {
+    id: 7,
+    name: "desktop development 2",
+    img: "/images/products/desktop dev2.jpg",
+  },
+
+  {
+    id: 8,
+    name: "digital marketing 2",
+    img: "/images/products/digital marketing 2.jpg",
+  },
+
+  { id: 9, name: "cloud computing 2", img: "/images/products/cloud comp2.jpg" },
+  {
+    id: 10,
+    name: "desktop development 3",
+    img: "/images/products/desktop dev3.png",
+  },
+
+  {
+    id: 11,
+    name: "mobile development2",
+    img: "/images/products/mobile develop 2.jpg",
+  },
 ];
