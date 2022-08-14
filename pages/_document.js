5f3ab1d1688e32d4fd66c6db71f3b0886038d38a
Ashfak Niloy <ashfakniloy56@gmail.com>
2022-08-14 22:53:30 +0600

    allowed http request

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          />
           <link rel="icon" href="/images/logo/shannonit.jpg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
